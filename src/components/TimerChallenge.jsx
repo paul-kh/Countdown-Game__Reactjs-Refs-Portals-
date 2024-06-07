8577fbb2e1f1ef6ff0b3eb5cb9b88c7c20241df0
Paul Chheang <phalcheat@gmail.com>
2024-06-07 16:30:58 -0500

    Used useRef() instead of normal variable

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,35 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+/*  The sharedTimer variable is shared among all instances of the TimerChalleng component
+    Every time new component instance was created, the shareTimer pointer will point to 
+    new timer created by setTimeOut() of that component instance.
+    This will lead to strange behavior when we want to independently clearTimer of each component instance */
+// let sharedTimer;
+
+// TO SOLVE THE ABOVE ISSUE, WE USE useRef() INSTEAD OF A REGULAR VARIABLE LIKE shareTimer
 
 export default function TimerChallenge({ title, targetTime }) {
+  // The below eachTimer object is conpoent-instance specific.
+  /* NOTE: The ref eachTimer object, unlike normal variable, 
+     will not be reset when component function re-executes. 
+     React store the ref */
+  const eachTimer = useRef();
+
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
-    setTimeout(() => {
+    //sharedTimer = setTimerExpired(true);
+    eachTimer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
+
     setTimerStarted(true);
+  }
+
+  function handleStop() {
+    // clearTimeout(sharedTimer);
+    clearTimeout(eachTimer.current);
   }
   return (
     <section className="challenge">
@@ -18,7 +39,7 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second {targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
