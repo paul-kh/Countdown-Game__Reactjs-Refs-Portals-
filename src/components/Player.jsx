4763830a3512460f9e389b92b60ed415bb319df6
Paul Chheang <phalcheat@gmail.com>
2024-06-07 11:55:03 -0500

    Used useRef() hook to get value of an <input> element instead of using 2-way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("unknown entity");
+  const playerName = useRef(); // useRef() returns a JS object
+
+  function handleClick() {
+    // The object (playerName) created by useRef(), with its special 'current' property,
+    // can access any native properties and methods of the referred/linked/connected HTML elements,
+    // in this case, the <input> element
+    setEnteredPlayerName(playerName.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        {/* useRef() can refer/connect/link any HTML element with a special prop "ref" */}
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
